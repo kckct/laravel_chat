ea40b29334b75aeb0f7d19f253b88f1a0689bc8a
kckct <ss010359@gmail.com>
2017-02-26 23:37:22 +0800

    修改config/broadcasting.php以及resources/assets/js/bootstrap.js設定

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -38,9 +38,11 @@ window.axios.defaults.headers.common = {
  * allows your team to easily build robust real-time web applications.
  */
 
-// import Echo from "laravel-echo"
+import Echo from "laravel-echo"
 
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
+window.Echo = new Echo({
+    broadcaster: 'pusher',
+    key: '50f7339c6a9334e324af',
+    cluster: 'mt1',
+    encrypted: true
+});
