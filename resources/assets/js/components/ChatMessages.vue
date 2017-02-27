51c99e39454e5efce176825e235b8616bf4cd56b
kckct <ss010359@gmail.com>
2017-02-27 13:26:08 +0800

    加入vue components, ChatMessages.vue, ChatForm.vue

---
+++ resources/assets/js/components/ChatMessages.vue
@@ -0,0 +1,20 @@
+<template>
+    <ul class="chat">
+        <li class="left clearfix" v-for="message in messages">
+            <div class="chat-body clearfix">
+                <div class="strong">
+                    {{ message.user.name }}
+                </div>
+            </div>
+            <p>
+                {{ message.message }}
+            </p>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        props: ['messages']
+    };
+</script>
