fa2121f129a6653d9f8c6112b43b08b84ba7bb52
kckct <ss010359@gmail.com>
2017-03-02 22:37:11 +0800

    fixed command error

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Command\SendChatMessage::class,
+        Commands\SendChatMessage::class,
     ];
 
     /**
